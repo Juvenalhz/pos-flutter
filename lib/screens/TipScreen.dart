9062bb548a0173d4dea332d04839823a32e790e8
Unknown <lcalvano@gmail.com>
2020-09-14 23:10:59 -0400

    show the base amount on the top of the tip screen

--- lib/screens/TipScreen.dart
+++ lib/screens/TipScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:pay/bloc/transaction/transaction_bloc.dart';
 import 'package:pay/models/trans.dart';
 
@@ -10,23 +11,28 @@ class TipScreen extends StatelessWidget {
   Trans trans;
 
   TipScreen(this.trans);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Stack(children: <Widget>[
-            Container(
-              height: 80,
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                gradient: LinearGradient(
-                  begin: Alignment(0.0, 0.6),
-                  end: Alignment(0.0, 0.0),
-                  colors: <Color>[
-                    Color(0xFF0D47A1),
-                    Colors.blue,
-                  ],
+    trans = ModalRoute.of(context).settings.arguments;
+
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: <Widget>[
+            Stack(children: <Widget>[
+              Container(
+                height: 80,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  gradient: LinearGradient(
+                    begin: Alignment(0.0, 0.6),
+                    end: Alignment(0.0, 0.0),
+                    colors: <Color>[
+                      Color(0xFF0D47A1),
+                      Colors.blue,
+                    ],
+                  ),
                 ),
                 child: Stack(children: <Widget>[
                   Positioned(
@@ -44,10 +50,26 @@ class TipScreen extends StatelessWidget {
                         child: BlocBuilder<TransactionBloc, TransactionState>(builder: (context, state) {
                           if (state is TransactionAddTip) {
                             int amount = state.trans.baseAmount;
-                            return Text(
-                              'Monto: $amount',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30),
+                            String formattedAmount;
+                            var formatter = new NumberFormat.currency(locale: 'eu', symbol: ' ', decimalDigits: 2);
+
+                            formattedAmount = formatter.format(amount / 100).trim();
+
+                            return Center(
+                              child: Column(
+                                children: [
+                                  Text(
+                                    'Monto:',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30),
+                                  ),
+                                  Text(
+                                    formattedAmount,
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30),
+                                  ),
+                                ],
+                              ),
                             );
                           } else {
                             return Text(
@@ -67,8 +89,8 @@ class TipScreen extends StatelessWidget {
                 color: Color(0xFF0D47A1),
               ),
               Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(topRight: Radius.circular(30), topLeft: Radius.circular(30)), color: Colors.white),
+                decoration:
+                    BoxDecoration(borderRadius: BorderRadius.only(topRight: Radius.circular(30), topLeft: Radius.circular(30)), color: Colors.white),
                 child: AmountEntry('Propina:', trans),
               ),
             ])),
