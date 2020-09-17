5f0211dd138e4fd94ebc82e853e34a7ddafda622
Unknown <lcalvano@gmail.com>
2020-09-16 23:03:59 -0400

    fill aid data to send to the BC module

--- android/app/src/main/kotlin/com/lccnet/pay/pinpad.kt
+++ android/app/src/main/kotlin/com/lccnet/pay/pinpad.kt
@@ -26,11 +26,67 @@ class pinpad : MethodChannel.MethodCallHandler {
             val aids : List<HashMap<String, Any?>>? = call.argument("aids")
             val pubKeys : List<HashMap<String, Any?>>? = call.argument("pubKeys")
 
-            Log.d("emv", emv.toString())
+            loadTables(emv, aids, pubKeys)
 
         } else {
             result.notImplemented()
         }
+    }
+
+    fun loadTables(emv : HashMap<String, Any?>?, aids : List<HashMap<String, Any?>>?, pubKeys : List<HashMap<String, Any?>>?){
+        val tables = arrayOfNulls<String>(150)
+        var i : Int
+
+        i = 0
+
+        if (aids != null) {
+            for((index, aid) in aids.withIndex()){
+
+                tables[i] = "01"    // fixed value
+                tables[i] += "314"  // record size
+                tables[i] += "1"    // aid table
+                tables[i] += "01"   // acquirer ID - fixed to 01 as only 1 acquirer is used
+                tables[i] += (index+1).toString().padStart(2, '0') // index
+                tables[i] += (aid["aid"].toString().length / 2).toString().padStart(2, '0')     // lengthe of aid
+                tables[i] += aid["aid"].toString().padEnd(32, '0')  // aid value
+                tables[i] += "01"   // 01 credit - 02 debit (right now there is not way to know if debit)
+                tables[i] += "".toString().padEnd(16, ' ')
+                tables[i] += "03"   // standar emv aid - fixed value
+                tables[i] += "%04x".format(aid["version"])  // terminal application 1
+                tables[i] += "0000"   // terminal application 2
+                tables[i] += "0000"   // terminal application 3
+                tables[i] += emv?.get("countryCode").toString().padStart(3, '0')
+                tables[i] += emv?.get("currencyCode").toString().padStart(3, '0')
+                tables[i] += "2"    // currency exponent
+                tables[i] += "".toString().padEnd(15, ' ')  // MID - optional
+                tables[i] += "5999" // merchant category code
+                tables[i] += "".toString().padEnd(8, ' ')  // TID - optional
+                tables[i] += emv?.get("terminalCapabilities").toString()
+                tables[i] += emv?.get("addTermCapabilities").toString().subSequence(0,10).toString()
+                tables[i] += emv?.get("terminalType").toString()
+                tables[i] += aid["tacDefault"].toString()
+                tables[i] += aid["tacDenial"].toString()
+                tables[i] += aid["tacOnline"].toString()
+                tables[i] += aid["floorLimit"].toString().padStart(8, '0')
+                tables[i] += "R"    // transaction category code
+                tables[i] += "0"    // contactles support
+                tables[i] += "0"    // cless type - 0 = not supported
+                tables[i] += "".toString().padEnd(8, '0')  // cless trans limit
+                tables[i] += "".toString().padEnd(8, '0')  // cless floor limit
+                tables[i] += "".toString().padEnd(8, '0')  // CVM Limit
+                tables[i] += "0000"     // paypass terminal version
+                tables[i] += "0"        // cless selection mode
+                tables[i] += aid["tdol"].toString().padEnd(40, '0') // TDOL
+                tables[i] += aid["ddol"].toString().padEnd(40, '0') // DDOL
+                tables[i] += "Y1Z1Y3Z3"  // fixed value - not used with newer emv specs
+                tables[i] += "".toString().padEnd(10, '0') // cless tac default
+                tables[i] += "".toString().padEnd(10, '0') // cless tac denial
+                tables[i] += "".toString().padEnd(10, '0') // cless tac online
+
+                i += 1
+            }
+        }
+
     }
 
 }
