2c3ece1a373ee6a107fe10043c20c167f5ea80fd
Leonardo Calvano <lcalvano@gmail.com>
2020-12-16 18:02:01 -0500

    show transaction data in a Card (data is not aligned correctly)

--- lib/screens/DetailReport.dart
+++ lib/screens/DetailReport.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -8,17 +10,10 @@ import 'package:pay/models/trans.dart';
 import 'package:pay/screens/splash.dart';
 
 class DetailReport extends StatelessWidget {
-  DetailReport();
 
   @override
   Widget build(BuildContext context) {
-    int total = 0;
-    String formattedAmount;
-    String formattedTip;
-    String formattedTotal;
     var formatter = new NumberFormat.currency(locale: 'eu', symbol: ' ', decimalDigits: 2);
-
-    formattedTotal = formatter.format(total / 100).trim();
 
     return SafeArea(
       child: Scaffold(
@@ -43,7 +38,9 @@ class DetailReport extends StatelessWidget {
                   IconButton(
                     color: Colors.white,
                     icon: Icon(Icons.arrow_back),
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
                   ),
                   Padding(
                     padding: const EdgeInsets.all(5.0),
@@ -62,23 +59,111 @@ class DetailReport extends StatelessWidget {
               ),
             ]),
             Expanded(
-                child: Stack(children: <Widget>[
-              Container(
-                color: Color(0xFF0D47A1),
-              ),
-              Container(
+              child: Stack(children: <Widget>[
+                Container(
+                  color: Color(0xFF0D47A1),
+                ),
+
+                Container(
+                  width: double.infinity,
+                  height: double.infinity,
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.only(topRight: Radius.circular(30), topLeft: Radius.circular(30)), color: Colors.white),
                   child: BlocBuilder<DetailReportBloc, DetailReportState>(builder: (context, state) {
                     if (state is DetailReportDataReady) {
-                      return ListView.builder(
-                        itemCount: state.transList.length,
-                        itemBuilder: (context, index) {
-                          formattedTotal = formatter.format(state.transList[index]['total'] / 100).trim();
-                          return ListTile(
-                            title: Text('${formattedTotal}'),
-                          );
-                        },
+                      return SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(2, 12, 1, 5),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      Spacer(flex: 1),
+                                      Text('Ticket', style: TextStyle(fontWeight: FontWeight.bold)),
+                                      Spacer(flex: 2),
+                                      Text('No. Tarjeta', style: TextStyle(fontWeight: FontWeight.bold)),
+                                      Spacer(flex: 3),
+                                      Text('Monto', style: TextStyle(fontWeight: FontWeight.bold)),
+                                      Spacer(flex: 1),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Spacer(flex: 1),
+                                      Text('Tipo', style: TextStyle(fontWeight: FontWeight.bold)),
+                                      Spacer(flex: 3),
+                                      Text('Fecha', style: TextStyle(fontWeight: FontWeight.bold)),
+                                      Spacer(flex: 3),
+                                      Text('Hora', style: TextStyle(fontWeight: FontWeight.bold)),
+                                      Spacer(flex: 5),
+
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                            ListView.builder(
+                              physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+                              scrollDirection: Axis.vertical,
+                              shrinkWrap: true,
+                              itemCount: state.transList.length,
+                              itemBuilder: (context, index) {
+                                Trans trans = Trans.fromMap(state.transList[index]);
+                                return Card(
+                                  child: Column(
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.fromLTRB(15, 4, 6, 2),
+                                        child: Row(
+                                          //mainAxisAlignment: MainAxisAlignment.center,
+                                          children: [
+                                            Text(trans.id.toString(), style: TextStyle(fontWeight: FontWeight.normal)),
+                                            Spacer(flex: 3),
+                                            Text(trans.maskedPAN, textAlign: TextAlign.left, style: TextStyle(
+                                              fontWeight: FontWeight.normal,
+                                              fontFeatures: [
+                                                FontFeature.tabularFigures()
+                                              ],
+                                            )),
+                                            Spacer(flex: 5),
+                                            Text(formatter.format(trans.total / 100),
+                                                style: TextStyle(
+                                                  fontWeight: FontWeight.normal,
+                                                  fontFeatures: [
+                                                    FontFeature.tabularFigures()
+                                                  ],
+                                                )
+                                            ),
+
+
+
+                                          ],
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.fromLTRB(6, 2, 6, 4),
+                                        child: Row(
+                                          children: [
+                                            Spacer(flex: 2),
+                                            Text(trans.type, style: TextStyle(fontWeight: FontWeight.normal)),
+                                            Spacer(flex: 3),
+                                            Text(DateFormat('dd/MM/yyyy').format(trans.dateTime), style: TextStyle(fontWeight: FontWeight.normal)),
+                                            Spacer(flex: 3),
+                                            Text(DateFormat('hh:mm:ss').format(trans.dateTime), style: TextStyle(fontWeight: FontWeight.normal)),
+                                            Spacer(flex: 1),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+
+                                );
+                              },
+                            ),
+                          ],
+                        ),
                       );
                     } else
                       return SplashScreen();
